94b0b129bfb2bf95b766d295e938dfd79fb341cf
shota <shotaro1707078@gmail.com>
2026-05-12 15:05:30 +0900

    100回ハロー

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -5,7 +5,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  
+    for (int i=0; i < 100; i++) {
+ std::cout << "Hello World!\n";
+    }
+   
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
